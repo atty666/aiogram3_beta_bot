b01b21b71d81a2246c4fd77f5120f55616f80ab1
denys <supriadkin.d@gmail.com>
2023-06-28 00:10:00 +0300

    Initial commit 2

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import asyncio
-import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from tokens import bot_token
 
-bot = Bot(token=os.getenv('BOT_TOKEN'))
+bot = Bot(token=bot_token)
 dp = Dispatcher()
+
+
+@dp.message(Command('start'))
+async def cmd_start(message: types.Message):
+    await message.answer('Hello my sweet friend!')
+
+
+@dp.message()
+async def wrong_cmd(message: types.Message):
+    await message.answer('Wrong command!')
 
 
 async def main():
